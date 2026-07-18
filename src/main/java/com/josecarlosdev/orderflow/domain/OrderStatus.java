365b77faaa20b1877c12d9be5f057a435d9b298e
JosCarRub <josecarcatarubio6@gmail.com>
2026-07-18 18:20:06 +0200

    feat(domain): add DomainException base class

---
+++ src/main/java/com/josecarlosdev/orderflow/domain/OrderStatus.java
@@ -0,0 +1,2 @@
+package com.josecarlosdev.orderflow.domain;public class OrderStatus {
+}
---
+++ src/main/java/com/josecarlosdev/orderflow/domain/exception/InvalidOrderStateException.java
@@ -0,0 +1,2 @@
+package com.josecarlosdev.orderflow.domain.exception;public class InvalidOrderStateException {
+}
