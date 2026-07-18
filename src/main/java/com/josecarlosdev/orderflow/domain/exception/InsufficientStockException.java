855f42f278799bedca97269f43e708df32495a6e
JosCarRub <josecarcatarubio6@gmail.com>
2026-07-18 18:22:25 +0200

    feat(domain): add Product entity with stock invariant

--- src/main/java/com/josecarlosdev/orderflow/domain/exception/InsufficientStockException.java
+++ src/main/java/com/josecarlosdev/orderflow/domain/exception/InsufficientStockException.java
@@ -1,2 +1,10 @@
-package com.josecarlosdev.orderflow.domain.exception;public class InsufficientStockException {
+package com.josecarlosdev.orderflow.domain.exception;
+
+
+public class InsufficientStockException extends DomainException {
+
+    public InsufficientStockException(Long productId, int available, int requested) {
+        super("Stock insuficiente para el producto %d: disponible %d, solicitado %d"
+                .formatted(productId, available, requested));
+    }
 }
