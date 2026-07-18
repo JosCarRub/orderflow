344ff2c7e70871ecd7a997f62126989b6d5a3014
JosCarRub <josecarcatarubio6@gmail.com>
2026-07-18 16:32:25 +0200

    chore: initialize Spring Boot project via Spring Initializr

---
+++ src/test/java/com/josecarlosdev/orderflow/TestOrderflowApplication.java
@@ -0,0 +1,11 @@
+package com.josecarlosdev.orderflow;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderflowApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
