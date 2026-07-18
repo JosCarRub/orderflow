f033b4b133372789e13db8f68fff62c6cb445fdd
JosCarRub <josecarcatarubio6@gmail.com>
2026-07-18 18:23:17 +0200

    feat(domain): add Order entity with state machine

--- src/main/java/com/josecarlosdev/orderflow/domain/exception/InvalidOrderStateException.java
+++ src/main/java/com/josecarlosdev/orderflow/domain/exception/InvalidOrderStateException.java
@@ -1,2 +1,11 @@
-package com.josecarlosdev.orderflow.domain.exception;public class InvalidOrderStateException {
+package com.josecarlosdev.orderflow.domain.exception;
+
+import com.josecarlosdev.orderflow.domain.OrderStatus;
+
+public class InvalidOrderStateException extends DomainException {
+
+    public InvalidOrderStateException(Long orderId, OrderStatus current, OrderStatus target) {
+        super("Transición inválida en el pedido %d: no se puede pasar de %s a %s"
+                .formatted(orderId, current, target));
+    }
 }
