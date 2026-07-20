6541c702b71ce0cba583aaad4dee7cc7b112fd6f
JosCarRub <josecarcatarubio6@gmail.com>
2026-07-20 16:30:22 +0200

    feat(web): add ApiPaths route constants

---
+++ src/main/java/com/josecarlosdev/orderflow/infrastructure/web/ApiPaths.java
@@ -0,0 +1,9 @@
+package com.josecarlosdev.orderflow.infrastructure.web;
+
+public final class ApiPaths {
+
+    private ApiPaths() {}
+
+    public static final String CHECKOUT = "/api/checkout";
+    public static final String STRIPE_WEBHOOK = "/api/webhooks/stripe";
+}
